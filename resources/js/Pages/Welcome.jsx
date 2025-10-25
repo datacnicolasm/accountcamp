791636b81193ff88a26c7bd93f272957bc3fc7b6
Nicolas Munoz <datac.nicolasm@gmail.com>
2025-10-24 22:22:32 -0500

    Hello world

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -165,7 +165,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
 
                                     <div className="pt-3 sm:pt-5">
                                         <h2 className="text-xl font-semibold text-black dark:text-white">
-                                            Laracasts
+                                            Laracasts NICOLAS
                                         </h2>
 
                                         <p className="mt-4 text-sm/relaxed">
